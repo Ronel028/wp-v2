0d3b05de82e993c7749bf63792744e9aec78f4b4
Ronel028 <floridaronel15@gmail.com>
2024-09-04 09:27:29 +0800

    update resume link

--- src/pages/components/sections/about.jsx
+++ src/pages/components/sections/about.jsx
@@ -57,7 +57,7 @@ const About = () => {
               Let's connect and create something amazing together!
             </p>
             <Link
-              href="https://drive.google.com/file/d/1OFPdQgZVnzPIANpZT3Hc8w-h_cp0z0ey/view?usp=sharing"
+              href="https://drive.google.com/file/d/1UwOuvF6T-MWw_qsiPwBKw2ex0CsXPMw7/view?usp=sharing"
               target="_blank"
               data-aos="fade-up"
               data-aos-delay="400"
